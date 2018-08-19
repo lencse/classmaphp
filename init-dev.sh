edeaf0a2ae32799766953f3708f716c89195117d
Levente Löki <leventeloki@gmail.com>
2018-08-19 12:36:37 +0200

    Removed fixture

--- init-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git clone -n git@github.com:symfony/symfony.git test-fixtures/symfony
-cd test-fixtures/symfony
-git checkout 75be8b747d06850136f181bac1bd67528eab6ce3
-rm -rf .git
